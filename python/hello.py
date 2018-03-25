ad56e1d44830f2f64e3e3c062d449921d5451b45
udit47 <connectudee@gmail.com>
2018-03-25 13:21:31 +0530

    changing files to add functions

--- python/hello.py
+++ python/hello.py
@@ -1,6 +1,12 @@
-print("This statement will be printed to say Hello world!")
+def expenses():
+    print("This function returns expense report!")
 
-i = 1
-print(i)
+def main():
+    print("This statement will be printed to say Hello world!")
+    i = 1
+    print(i)
+    print("One more change done!")
+    expenses()
 
-print("One more change done!")
+if __name__ == "__main__":
+    main()
