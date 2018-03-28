a44f0956b4c6e37fd8ec68637196ef2e66fc4182
udit47 <connectudee@gmail.com>
2018-03-28 10:34:41 +0530

    added two new functions for boi

--- python/hello.py
+++ python/hello.py
@@ -1,12 +1,20 @@
 def expenses():
     print("This function returns expense report!")
 
+def goodBoi():
+    print("This is a good boi!")
+
+def badBoi():
+    print("This is a bad boi!")
+
 def main():
     print("This statement will be printed to say Hello world!")
     i = 1
     print(i)
     print("One more change done!")
     expenses()
+    goodBoi()
+    badBoi()
 
 if __name__ == "__main__":
     main()
