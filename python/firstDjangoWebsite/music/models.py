04073c16b39b30348e81848c215719318cf9e4ec
udit47 <connectudee@gmail.com>
2018-03-29 10:58:04 +0530

    Song model added to databaase and configured it to access from admin site

--- python/firstDjangoWebsite/music/models.py
+++ python/firstDjangoWebsite/music/models.py
@@ -11,6 +11,9 @@ class Album(models.Model):
         return self.album_title + ' - ' + self.artist
 
 class Song(models.Model):
-    album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.song_title + '.' + self.file_type
--- python/firstDjangoWebsite/music/views.py
+++ python/firstDjangoWebsite/music/views.py
@@ -1,12 +1,16 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 from .models import Album
+from django.shortcuts import render
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    return HttpResponse(html)
+    context = {'all_albums' : all_albums}
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album_id : " + str(album_id) + "</h2>" )
+    try:
+        album = Album.objects.get(id = album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album' : album})
