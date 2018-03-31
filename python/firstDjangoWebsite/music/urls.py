6b86343ac4a1b5f464c5c5f4b3c0a9c11c3758f9
udit47 <connectudee@gmail.com>
2018-03-31 20:54:05 +0530

    added song favorite code, exceptions,  changed album details page, new view for favorite

--- python/firstDjangoWebsite/music/urls.py
+++ python/firstDjangoWebsite/music/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path,re_path
 from . import views
 
+app_name = "music"
+
 urlpatterns = [
     # /music/
     path('', views.index, name='index'),
 
     # /music/71/
     re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/71/favorite/
+    re_path(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
--- python/firstDjangoWebsite/music/views.py
+++ python/firstDjangoWebsite/music/views.py
@@ -1,6 +1,5 @@
-from django.http import Http404
 from .models import Album
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -8,9 +7,22 @@ def index(request):
     context = {'all_albums' : all_albums}
     return render(request, 'music/index.html', context)
 
+
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(id = album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album' : album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        song = album.song_set.get(pk=request.POST['song'])
+    except:
+        return render(request, 'music/detail.html', {
+            'album' : album,
+            'error_messge' : 'You did not select a valid song'
+        })
+    else:
+        song.is_favorite = True
+        song.save()
+        return render(request, 'music/detail.html', {'album' : album})
